4a89cf6a96559d33999a44a9e79517d7a8a49f44
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2023-10-26 14:25:50 -0700

    naampy script
    naampy script to predict 2015 and 2020 incumbent sex

--- scripts/02_sarpanch_analysis.R
+++ scripts/02_sarpanch_analysis.R
@@ -8,62 +8,14 @@ library(stringr)
 library(here)
 library(kableExtra)
 
+
 # Load data
 data_dir <- here("..", "data/sarpanch_election_data")
 sp_fin_file <- here(data_dir, "sp_2005_2010_2015_2020_fin.csv")
 raj_panch <- readr::read_csv(sp_fin_file)
 tables_folder <- here("tables")
 
-# Transition Matrces ------------------------------------------------------
-
-trans_05_10 <- table(raj_panch$reservation_2005, raj_panch$reservation_2010)
-trans_10_15 <- table(raj_panch$reservation_2010, raj_panch$reservation_2015)
-trans_15_20 <- table(raj_panch$reservation_2015, raj_panch$reservation_2020)
-
-# blank dataframe to store results
-trans_matrices_store <- data.frame(
-     "Year Comparison" = c("2005-2010", "2010-2015", "2015-2020"),
-     "Transition Matrix" = c(
-          kable(trans_05_10, format = "latex", escape = FALSE, booktabs = TRUE),
-          kable(trans_10_15, format = "latex", escape = FALSE, booktabs = TRUE),
-          kable(trans_15_20, format = "latex", escape = FALSE, booktabs = TRUE)
-     )
-)
-
-# TeX export Beautify this later
-trans_matrices_table <- trans_matrices_store %>%
-     kable("latex", escape = FALSE, booktabs = TRUE, caption = "Transition Matrices")
-cat(trans_matrices_table, file = here("tables", "tran_matrices.tex"))
-
-
-# Chi-Squared Test --------------------------------------------------------
-
-chi_sq_test_05_10 <- chisq.test(trans_05_10, simulate.p.value = TRUE)
-chi_sq_test_10_15 <- chisq.test(trans_10_15, simulate.p.value = TRUE)
-chi_sq_test_15_20 <- chisq.test(trans_15_20, simulate.p.value = TRUE)
-
-# blank dataframe to store results
-
-results_store <- data.frame(
-     "Year Comparison" = c("2005-2010", "2010-2015", "2015-2020"),
-     "Chi-Squared Test Result" = c(
-          format(chi_sq_test_05_10$p.value, scientific = TRUE, digits = 2),
-          format(chi_sq_test_10_15$p.value, scientific = TRUE, digits = 2),
-          format(chi_sq_test_15_20$p.value, scientific = TRUE, digits = 2)
-     )
-)
-
-#teX export Beautify this later
-chisq_table <- results_store %>%
-     kable("latex", escape = FALSE, booktabs = TRUE, caption = "Chi-Squared Test Results") %>%
-     kable_styling(full_width = FALSE, position = "center")
-cat(chisq_table, file = here("tables", "chisq_table.tex"))
-
-
-#check chisquared results when you are actually awake! 
-
 # Create reservation dummies, caste group dummies -------------------------
-
 
 # inefficient but does the job
 raj_panch <- raj_panch %>%
@@ -85,10 +37,77 @@ raj_panch <- raj_panch %>%
           sometimes_treated = ifelse(treat_2005 + treat_2010 + treat_2015 > 0, 1, 0),
           treatment_intensity = treat_2005 + treat_2010 + treat_2015
      )
+
 table(raj_panch$sometimes_treated)
 table(raj_panch$never_treated)
 table(raj_panch$always_treated)
 table(raj_panch$treatment_intensity)
+
+
+# Transition Matrces ------------------------------------------------------
+
+trans_05_10 <- table(raj_panch$reservation_2005, raj_panch$reservation_2010)
+trans_10_15 <- table(raj_panch$reservation_2010, raj_panch$reservation_2015)
+trans_15_20 <- table(raj_panch$reservation_2015, raj_panch$reservation_2020)
+
+trans_05_15 <- table(raj_panch$reservation_2005, raj_panch$reservation_2015)
+trans_05_20 <- table(raj_panch$reservation_2005, raj_panch$reservation_2020)
+
+# trans_05_10 <- table(raj_panch$treat_2005, raj_panch$treat_2010)
+# trans_10_15 <- table(raj_panch$treat_2010, raj_panch$treat_2015)
+# trans_15_20 <- table(raj_panch$treat_2015, raj_panch$treat_2020)
+
+
+# blank dataframe to store results
+trans_matrices_store <- data.frame(
+     "Year Comparison" = c("2005-2010", "2010-2015", "2015-2020"),
+     "Transition Matrix" = c(
+          kable(trans_05_10, format = "latex", escape = FALSE, booktabs = TRUE),
+          kable(trans_10_15, format = "latex", escape = FALSE, booktabs = TRUE),
+          kable(trans_15_20, format = "latex", escape = FALSE, booktabs = TRUE)
+     )
+)
+
+# TeX export Beautify this later
+trans_matrices_table <- trans_matrices_store %>%
+     kable("latex", escape = FALSE, booktabs = TRUE, caption = "Transition Matrices")
+cat(trans_matrices_table, file = here("tables", "tran_matrices.tex"))
+
+
+# Chi-Squared Test --------------------------------------------------------
+
+chi_sq_test_05_10 <- chisq.test(trans_05_10)
+chi_sq_test_05_10
+chi_sq_test_10_15 <- chisq.test(trans_10_15)
+chi_sq_test_10_15
+chi_sq_test_15_20 <- chisq.test(trans_15_20)
+chi_sq_test_15_20
+
+#p-values are nearly identical, check again!  
+
+
+
+# blank dataframe to store results
+
+results_store <- data.frame(
+     "Year Comparison" = c("2005-2010", "2010-2015", "2015-2020"),
+     "Chi-Squared Test Result" = c(
+          format(chi_sq_test_05_10$p.value, scientific = TRUE, digits = 2),
+          format(chi_sq_test_10_15$p.value, scientific = TRUE, digits = 2),
+          format(chi_sq_test_15_20$p.value, scientific = TRUE, digits = 2)
+     )
+)
+
+#teX export Beautify this later
+chisq_table <- results_store %>%
+     kable("latex", escape = FALSE, booktabs = TRUE, caption = "Chi-Squared Test Results") %>%
+     kable_styling(full_width = FALSE, position = "center")
+cat(chisq_table, file = here("tables", "chisq_table.tex"))
+
+
+#check chisquared results when you are actually awake! 
+
+
 
 
 # Regressions -------------------------------------------------------------
@@ -97,4 +116,4 @@ table(raj_panch$treatment_intensity)
 # immediate effect
 
 m_05_10 <- lm((raj_panch$sex_2010 =="F") ~ treat_2005, data = raj_panch)
-m_05_10_fe <- (lm((sex_2010 =="F") ~ treat_2005 + factor(gp_2010), data = raj_panch))
+m_05_10_fe <- lm((sex_2010 =="F") ~ treat_2005 + factor(gp_2010), data = raj_panch)
