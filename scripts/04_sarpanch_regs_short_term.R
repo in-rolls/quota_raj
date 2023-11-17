82f39c23522c4e2454e90acf7781d749c4a6b4e0
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2023-11-17 15:52:43 -0800

    gp fixed effects
    1. models with full fixed effects specifications

--- scripts/04_sarpanch_regs_short_term.R
+++ scripts/04_sarpanch_regs_short_term.R
@@ -61,41 +61,87 @@ raj_panch <- raj_panch %>%
 m_05_10 <- feols((sex_2010 =="F") ~ treat_2005, data = filter(raj_panch, treat_2010 == 0))
 summary(m_05_10)
 
-m_05_10_fe <- feols((sex_2010 =="F") ~ treat_2005 | fe_key_2010,  data = filter(raj_panch, treat_2010 == 0) )
-summary(m_05_10_fe)
+m_05_10_dfe <- feols((sex_2010 =="F") ~ treat_2005 | dist_name_2010, vcov = ~gp_2010, data = filter(raj_panch, treat_2010 == 0) )
+summary(m_05_10_dfe)
 
-m_05_10_clust <- feols((sex_2010 =="F") ~ treat_2005 | fe_key_2010, vcov = ~cluster_key_2010, data = filter(raj_panch, treat_2010 == 0))
-summary(m_05_10_clust)
+m_05_10_psfe <- feols((sex_2010 =="F") ~ treat_2005 | dist_name_2010 + samiti_name_2010, vcov = ~gp_2010,  data = filter(raj_panch, treat_2010 == 0))
+summary(m_05_10_psfe)
 
 
 # TeX
-models_05_10_list <- list(m_05_10, m_05_10_fe, m_05_10_clust)
-etable(models_05_10_list, tex=TRUE, style.df = style.df, file = here("..", "tables", "models_05_10.tex"), title = "Short-run Effects 2005 -- 2010",    replace = TRUE)
+models_05_10_list <- list(m_05_10, m_05_10_dfe, m_05_10_psfe)
+
+etable(models_05_10_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = here("..", "tables", "models_05_10.tex"), 
+       dict = c( 'sex_2010 == "F"' = "2010 rep is a woman in an open seat", 
+                 "treat_2005" = "Quota Treatment in 2005", 
+                 "dist_name_2010" = "District (2010)",
+                 "samiti_name_2010" = "Panchayat Samiti (2010)",
+                 "gp_2010" = "Gram Panchayat (2010)"), #  notes = "Robust standard errors clustered at gram panchayat level",
+       signif.code = NA,
+   
+       replace = TRUE)
+
+# etable(models_05_10_list, tex=TRUE, style.df = style.df, file = here("..", "tables", "models_05_10.tex"),     replace = TRUE) #title = "Short-run Effects 2005 -- 2010",
 
 # 2010 ---> 2015 ----------------------------------------------------------
 
 m_10_15 <- feols((sex_manual_2015 == "F") ~ treat_2010, data = filter(raj_panch, treat_2015 == 0))
 summary(m_10_15)
 
-m_10_15_fe <- feols((sex_manual_2015 == "F") ~ treat_2010 | fe_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_10_15_fe)
+m_10_15_dfe <- feols((sex_manual_2015 == "F") ~ treat_2010 | dist_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_10_15_dfe)
 
-m_10_15_clust <- feols((sex_manual_2015 == "F") ~ treat_2010 | fe_key_2015, vcov = ~cluster_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_10_15_clust)
+m_10_15_psfe <- feols((sex_manual_2015 == "F") ~ treat_2010 | dist_name_2015 + samiti_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_10_15_psfe)
 
-models_10_15_list <- list(m_10_15, m_10_15_fe, m_10_15_clust)
-etable(models_10_15_list, tex=TRUE, style.df = style.df, file = here("..", "tables", "models_10_15.tex"), title = "Short-run Effects 2010 -- 2015",    replace = TRUE)
+
+models_10_15_list <- list(m_10_15, m_10_15_dfe, m_10_15_psfe)
+
+
+etable(models_10_15_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = here("..", "tables", "models_10_15.tex"), 
+       dict = c( 'sex_manual_2015 == "F"' = "2015 rep is a woman in an open seat", 
+                 "treat_2010" = "Quota Treatment in 2010", 
+                 "dist_name_2015" = "District (2015)",
+                 "samiti_name_2015" = "Panchayat Samiti (2015)",
+                 "gp_2015" = "Gram Panchayat (2015)"), #  notes = "Robust standard errors clustered at gram panchayat level",
+       signif.code = NA,
+       replace = TRUE)
+
 
 # 2015 ---> 2020 ----------------------------------------------------------
 
 m_15_20 <- feols((sex_2020 == "F") ~ treat_2015, data = filter(raj_panch, treat_2020 == 0))
-summary(m_10_15)
+summary(m_15_20)
 
-m_15_20_fe <- feols((sex_2020 == "F") ~ treat_2015 | fe_key_2020, data = filter(raj_panch, treat_2020 == 0))
-summary(m_15_20_fe)
+m_15_20_dfe <- feols((sex_2020 == "F") ~ treat_2015 | district_2020, vcov = ~gp_2020, data = filter(raj_panch, treat_2020 == 0))
+summary(m_15_20_dfe)
 
-m_15_20_clust <- feols((sex_2020 == "F") ~ treat_2015 | fe_key_2020, vcov = ~cluster_key_2020, data = filter(raj_panch, treat_2020 == 0))
-summary(m_15_20_clust)
+m_15_20_psfe <- feols((sex_2020 == "F") ~ treat_2015 | district_2020 + ps_2020, vcov = ~gp_2020, data = filter(raj_panch, treat_2020 == 0))
+summary(m_15_20_psfe)
 
-models_15_20_list <- list(m_15_20, m_15_20_fe, m_15_20_clust)
-etable(models_15_20_list, tex=TRUE, style.df = style.df, file = here("..", "tables", "models_15_20.tex"), title = "Short-run Effects 2015 -- 2020",    replace = TRUE)
+
+models_15_20_list <- list(m_15_20, m_15_20_dfe, m_15_20_psfe)
+
+
+etable(models_15_20_list, 
+      # vcov = list(NULL, ~gp_2020, ~gp_2020),
+      # se = c("standard", "cluster","cluster"),
+      style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+      interaction.combine = " $\times $ ",
+       tex = TRUE, 
+       file = here("..", "tables", "models_15_20.tex"), 
+       dict = c( 'sex_2020 == "F"' = "2020 rep is a woman in an open seat", 
+                 "treat_2015" = "Quota Treatment in 2015", 
+                 "district_2020" = "District (2020)",
+                 "ps_2020" = "Panchayat Samiti (2020)",
+                 "gp_2020" = "Gram Panchayat (2020)"),
+                 #"SE_Type" = "S.E. Type"), #  notes = "Robust standard errors clustered at gram panchayat level"
+       replace = TRUE)
--- scripts/05_sarpanch_regs_inter_term.R
+++ scripts/05_sarpanch_regs_inter_term.R
@@ -54,88 +54,136 @@ raj_panch <- raj_panch %>%
           cluster_key_2020 = paste(district_2020, ps_2020, gp_2020)
      )
 
-# Intermediate Effects of Quotas 05_10_15 ---------------------------------
-
-# 10--->15
-
-# define fixedeffect keys
-raj_panch <- raj_panch %>% 
-     mutate(fe_key_2015 = paste(dist_name_2015,samiti_name_2015))
-raj_panch <- raj_panch %>% 
-     mutate(cluster_key_2015 = paste(dist_name_2015,samiti_name_2015, gp_2015))
-
-m_10_15 <- feols((sex_manual_2015 == "F") ~ treat_2010, data = filter(raj_panch, treat_2015 == 0))
-summary(m_10_15)
-
-m_10_15_fe <- feols((sex_manual_2015 == "F") ~ treat_2010 | fe_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_10_15_fe)
-
-m_10_15_clust <- feols((sex_manual_2015 == "F") ~ treat_2010 | fe_key_2015, vcov = ~cluster_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_10_15_clust)
-
-models_10_15_list <- list(m_10_15, m_10_15_fe, m_10_15_clust)
-etable(models_10_15_list, tex=TRUE, style.df = style.df, file = here("..", "tables", "models_10_15.tex"), title = "Short-run Effects 2010 -- 2015",    replace = TRUE)
 
 # Intermediate Effect treat_2005 * treat_2010 -----------------------------
 # interaction of treat_2005 and treat_2010
 
+#ecologiucal thing. Constsat capturing the eological trend. Treat_2010. Next step always trreated vs never treated. keep these units and compare. 
+
 m_05_10_15 <- feols((sex_manual_2015 == "F") ~ treat_2010 * treat_2005, data = filter(raj_panch, treat_2015 == 0))
 summary(m_05_10_15)
 
-m_05_10_15_fe <- feols((sex_manual_2015 == "F") ~ treat_2010 * treat_2005  | fe_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_05_10_15_fe)
+m_05_10_15_dfe <- feols((sex_manual_2015 == "F") ~ treat_2010 * treat_2005  | dist_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_05_10_15_dfe)
 
-m_05_10_15_clust <- feols((sex_manual_2015 == "F") ~ treat_2010 * treat_2005  | fe_key_2015, vcov = ~cluster_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_05_10_15_clust)
+m_05_10_15_psfe <- feols((sex_manual_2015 == "F") ~ treat_2010 * treat_2005  | dist_name_2015 + samiti_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_05_10_15_psfe)
 
-# TeX
-models_05_10_15_list <- list(m_05_10_15, m_05_10_15_fe, m_05_10_15_clust)
-etable(models_05_10_15_list, tex = TRUE, file = here("..", "tables", "intermediate_05*10.tex"), title = "Interaction Effects Treat 2005 * 2010", placement = "htbp", replace = TRUE)
+m_05_10_15_gpfe <- feols((sex_manual_2015 == "F") ~ treat_2010 * treat_2005  | dist_name_2015 + samiti_name_2015 + gp_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_05_10_15_gpfe)
+
+models_05_10_15_list <- list(m_05_10_15, m_05_10_15_dfe, m_05_10_15_psfe, m_05_10_15_gpfe)
+
+etable(models_05_10_15_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = here("..", "tables", "intermediate_05X10.tex"), 
+       dict = c( 'sex_manual_2015 == "F"' = "2015 rep is a woman in an open seat", 
+                 "treat_2010" = "Quota in 2010", 
+                 "treat_2005" = "Quota in 2005",
+                 "dist_name_2015" = "District (2015)",
+                 "samiti_name_2015" = "Panchayat Samiti (2015)",
+                 "gp_2015" = "Gram Panchayat (2015)"), #  notes = "Robust standard errors clustered at gram panchayat level",
+       signif.code = NA,
+       replace = TRUE)
+
+
+# etable(models_05_10_15_list, tex = TRUE, file = here("..", "tables", "intermediate_05*10.tex"), title = "Interaction Effects Treat 2005 * 2010", placement = "htbp", replace = TRUE)
 
 # Intermediate Always Treated 
 
 m_inter_always <- feols((sex_manual_2015 == "F") ~ inter_always_treated, data = filter(raj_panch, treat_2015 == 0))
 summary(m_inter_always)
 
-m_inter_always_fe <- feols((sex_manual_2015 == "F") ~ inter_always_treated  |  fe_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_inter_always_fe)
+m_inter_always_dfe <- feols((sex_manual_2015 == "F") ~ inter_always_treated  |  dist_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_always_dfe)
 
-m_inter_always_clust <- feols((sex_manual_2015 == "F") ~ inter_always_treated  | fe_key_2015, vcov = ~cluster_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_inter_always_clust)
+m_inter_always_psfe <- feols((sex_manual_2015 == "F") ~ inter_always_treated  | dist_name_2015 + samiti_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_always_psfe)
+
+m_inter_always_gpfe <- feols((sex_manual_2015 == "F") ~ inter_always_treated  | dist_name_2015 + samiti_name_2015 + gp_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_always_psfe)
+
 
 
 # TeX
-m_inter_always_list <- list(m_inter_always, m_inter_always_fe, m_inter_always_clust)
-etable(m_inter_always_list, tex = TRUE, file = here("..", "tables", "inter_always_treated.tex"), title = "Intermediate Effects: Treated in 2005 and 2010", placement = "htbp",  replace = TRUE)
+m_inter_always_list <- list(m_inter_always, m_inter_always_dfe, m_inter_always_psfe, m_inter_always_gpfe)
 
+etable(m_inter_always_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = here("..", "tables", "inter_always_treated.tex"), 
+       dict = c( 'sex_manual_2015 == "F"' = "2015 rep is a woman in an open seat", 
+                 "inter_always_treated" = "Always treated (quota in 2005, \& 2010)", 
+                 "treat_2005" = "Quota in 2005",
+                 "dist_name_2015" = "District (2015)",
+                 "samiti_name_2015" = "Panchayat Samiti (2015)",
+                 "gp_2015" = "Gram Panchayat (2015)"), #  notes = "Robust standard errors clustered at gram panchayat level",
+       signif.code = NA,
+       replace = TRUE)
 
 # Intermediate Never Treated 
 
 m_inter_never <- feols((sex_manual_2015 == "F") ~ inter_never_treated, data = filter(raj_panch, treat_2015 == 0))
 summary(m_inter_never)
 
-m_inter_never_fe <- feols((sex_manual_2015 == "F") ~ inter_never_treated  | fe_key_2015,  data = filter(raj_panch, treat_2015 == 0))
-summary(m_inter_never_fe)
+m_inter_never_dfe <- feols((sex_manual_2015 == "F") ~ inter_never_treated  | dist_name_2015, vcov = ~gp_2015,   data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_never_dfe)
 
-m_inter_never_clust <- feols((sex_manual_2015 == "F") ~ inter_never_treated  | fe_key_2015, vcov = ~cluster_key_2015, data = filter(raj_panch, treat_2015 == 0))
-summary(m_inter_never_clust)
+m_inter_never_psfe <- feols((sex_manual_2015 == "F") ~ inter_never_treated  | dist_name_2015 + samiti_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_never_psfe)
+
+m_inter_never_gpfe <- feols((sex_manual_2015 == "F") ~ inter_never_treated  | dist_name_2015 + samiti_name_2015, vcov = ~gp_2015, data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_never_gpfe)
 
 # TeX
-m_inter_never_list <- list(m_inter_never, m_inter_never_fe, m_inter_never_clust)
-etable(m_inter_never_list, tex = TRUE, file = here("..", "tables", "inter_never_treated.tex"), title = "Intermediate Effects: Never treated in 2005 and 2010", placement = "htbp",  replace = TRUE)
+m_inter_never_list <- list(m_inter_never, m_inter_never_dfe, m_inter_never_psfe, m_inter_never_gpfe)
 
+
+etable(m_inter_never_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = here("..", "tables", "inter_never_treated.tex"), 
+       dict = c( 'sex_manual_2015 == "F"' = "2015 rep is a woman in an open seat", 
+                 "inter_never_treated" = "Never treated (no quota in 2005, \& 2010)", 
+                 "treat_2005" = "Quota in 2005",
+                 "dist_name_2015" = "District (2015)",
+                 "samiti_name_2015" = "Panchayat Samiti (2015)",
+                 "gp_2015" = "Gram Panchayat (2015)"), #  notes = "Robust standard errors clustered at gram panchayat level",
+       signif.code = NA,
+       replace = TRUE)
 
 # Intermediate Sometimes Treated
 
 m_inter_sometimes <- feols((sex_manual_2015 == "F") ~ inter_sometimes_treated,  data = filter(raj_panch, treat_2015 == 0))
 summary(m_inter_sometimes) 
 
-m_inter_sometimes_fe <- feols((sex_manual_2015 == "F") ~ inter_sometimes_treated  | fe_key_2015,  data = filter(raj_panch, treat_2015 == 0))
-summary(m_inter_sometimes_fe)
+m_inter_sometimes_dfe <- feols((sex_manual_2015 == "F") ~ inter_sometimes_treated  | dist_name_2015, vcov = ~gp_2015,  data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_sometimes_dfe)
 
-m_inter_sometimes_clust <- feols((sex_manual_2015 == "F") ~ inter_sometimes_treated  | fe_key_2015, vcov = ~cluster_key_2015,  data = filter(raj_panch, treat_2015 == 0))
-summary(m_inter_sometimes_clust)
+m_inter_sometimes_psfe <- feols((sex_manual_2015 == "F") ~ inter_sometimes_treated  | dist_name_2015 + samiti_name_2015, vcov = ~gp_2015,  data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_sometimes_psfe)
+
+m_inter_sometimes_gpfe <- feols((sex_manual_2015 == "F") ~ inter_sometimes_treated  | dist_name_2015 + samiti_name_2015 + gp_2015, vcov = ~gp_2015,  data = filter(raj_panch, treat_2015 == 0))
+summary(m_inter_sometimes_gpfe)
+
 
 # TeX
-m_inter_sometimes_list <- list(m_inter_sometimes, m_inter_sometimes_fe, m_inter_sometimes_clust)
-etable(m_inter_sometimes_list, tex = TRUE, file = here("..", "tables", "inter_sometimes_treated.tex"), title = "Intermediate Effects: Treated in 2005 Or in 2010", placement = "htbp",  replace = TRUE)
+m_inter_sometimes_list <- list(m_inter_sometimes, m_inter_sometimes_dfe, m_inter_sometimes_psfe, m_inter_sometimes_gpfe)
+
+etable(m_inter_sometimes_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = here("..", "tables", "inter_sometimes_treated.tex"), 
+       dict = c( 'sex_manual_2015 == "F"' = "2015 rep is a woman in an open seat", 
+                 "inter_sometimes_treated" = "Sometimes treated (quota in 2005 or 2010)", 
+                 "treat_2005" = "Quota in 2005",
+                 "dist_name_2015" = "District (2015)",
+                 "samiti_name_2015" = "Panchayat Samiti (2015)",
+                 "gp_2015" = "Gram Panchayat (2015)"), #  notes = "Robust standard errors clustered at gram panchayat level",
+       signif.code = NA,
+       replace = TRUE)
