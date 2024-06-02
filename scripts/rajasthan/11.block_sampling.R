258d44494b35ae861e9618349fded94255b24861
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2024-06-01 22:21:31 -0700

    Revert "block sampling fix"
    This reverts commit 192ffab1265a6a92c73b38f7193bafd652d9dc74.

--- scripts/rajasthan/11.block_sampling.R
+++ scripts/rajasthan/11.block_sampling.R
@@ -22,19 +22,17 @@ raj_sarpanch <- raj_sarpanch %>%
 raj_sarpanch <- raj_sarpanch %>% 
      mutate(treat_2020 = ifelse(raj_sarpanch$CategoryOfGramPanchyat %in% c("General (Woman)", "OBC (Woman)", "SC (Woman)", "ST (Woman)"), 1, 0))
 
-treated_units_df <- raj_sarpanch %>% 
-     filter(treat_2020 == 1)
+
 
 n_districts <- length(unique(raj_sarpanch$District))
-n_cat_gp <- length(unique(treated_units_df$CategoryOfGramPanchyat))
-samples_per_group <- ceiling(800 / (n_districts * n_cat_gp))
+n_treatments <- length(unique(raj_sarpanch$treat_2020))
+samples_per_group <- ceiling(500 / (n_districts * n_treatments))
 
- 
+
 set.seed(12091986)  
-sampled_mobile_nos <- treated_units_df %>%
-     group_by(District, CategoryOfGramPanchyat) %>%
-     sample_n(min(samples_per_group, n()), replace = FALSE) 
+sampled_mobile_nos <- raj_sarpanch %>%
+     group_by(District, treat_2020) %>%
+     sample_n(samples_per_group, replace = FALSE) %>%
+     ungroup()
 
-
-     
 # write.csv(sampled_mobile_nos, "sampled_mobile_nos.csv", row.names = FALSE)
