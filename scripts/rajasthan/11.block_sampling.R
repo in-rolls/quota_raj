5dd19ce44377563184c52828a6942a28028a7718
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2024-06-01 22:29:59 -0700

    fix sampling code
    -fixed block sampling code to only sample from treated units
    
    Co-Authored-By: Rajashekar Chintalapati <rajshekar.ch@gmail.com>

--- scripts/rajasthan/11.block_sampling.R
+++ scripts/rajasthan/11.block_sampling.R
@@ -23,14 +23,17 @@ raj_sarpanch <- raj_sarpanch %>%
      mutate(treat_2020 = ifelse(raj_sarpanch$CategoryOfGramPanchyat %in% c("General (Woman)", "OBC (Woman)", "SC (Woman)", "ST (Woman)"), 1, 0))
 
 
+treated_units_df <- raj_sarpanch %>% 
+     filter(treat_2020 == 1)
+
 
 n_districts <- length(unique(raj_sarpanch$District))
 n_treatments <- length(unique(raj_sarpanch$treat_2020))
-samples_per_group <- ceiling(500 / (n_districts * n_treatments))
+samples_per_group <- ceiling(1000 / (n_districts * n_treatments))
 
 
 set.seed(12091986)  
-sampled_mobile_nos <- raj_sarpanch %>%
+sampled_mobile_nos <- treated_units_df %>%
      group_by(District, treat_2020) %>%
      sample_n(samples_per_group, replace = FALSE) %>%
      ungroup()
