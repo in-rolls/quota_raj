34f81bdf73399f64fcb0a4368ec3dfdd8cf87c69
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2024-07-22 10:11:33 -0700

    running_rates, Raj open seat

--- scripts/rajasthan/12_contesting_effects.R
+++ scripts/rajasthan/12_contesting_effects.R
@@ -38,10 +38,53 @@ raj_panch <- raj_panch %>% mutate(key_2020 = tolower(key_2020))
 
 
 contest_prop <- contest %>% select(key_2020, prop_women)
-
+contest_prop <- contest_prop %>% distinct()
 raj_panch <- raj_panch %>% left_join(contest_prop, by = "key_2020") #raj_panch$contest_prop has proportion of female contestants. Logic test, prop_women should be 1 in treat_2020==1
 
 test <- raj_panch %>% select(treat_2020, prop_women)
 test <- test %>% filter(treat_2020==1)
 test <- test %>% filter(prop_women<1) #about 1% observations. Safe to drop?
 
+
+# Contestant models -------------------------------------------------------
+
+     
+m_always_lt_gpfe <- feols(prop_women ~ always_treated  | I(paste0(district_2020, ps_2020)) , data = filter(raj_panch, treat_2020 == 0))
+summary(m_always_lt_gpfe)
+
+m_never_gpfe <- feols(prop_women ~ never_treated  | I(paste0(district_2020, ps_2020))  ,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_never_gpfe)
+
+m_sometimes_gpfe <- feols(prop_women ~ sometimes_treated  | I(paste0(district_2020, ps_2020))  ,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_sometimes_gpfe)
+
+
+raj_max_contrast_poster <- filter(raj_panch, treat_2020 == 0 & (never_treated == 1 | always_treated == 1))
+
+raj_max_contrast_poster <- raj_max_contrast_poster %>%
+     mutate(treatment_category = ifelse(never_treated == 1, "Never Treated", "Always Treated"))
+
+
+raj_max_contrast_poster$treatment_category <- factor(raj_max_contrast_poster$treatment_category, levels = c("Never Treated", "Always Treated"))
+
+m_never_v_always_gpfe <- feols(prop_women ~ treatment_category | I(paste0(district_2020, ps_2020)),  data = raj_max_contrast_poster)
+summary(m_never_v_always_gpfe) 
+
+models_contests_list <- list(m_always_lt_gpfe, m_never_gpfe, m_sometimes_gpfe,m_never_v_always_gpfe)
+
+
+etable(models_contests_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = "tables/raj_contesting.tex",
+       dict = c( 'treat_2005' = "Treatment 2005",
+                 'prop_women' == "Proportion of women running in an open seat",
+                 'treat_2010' = "Treatment  2010",
+                 'treat_2015' = "Treatment 2015",
+                 'treat_2020' = "Treatment 2020",
+                 "treatment_categoryAlways Treated" = " Max Contrast ($WWW$ v. $OO0$)", 
+                 "never_treated" = "Never treated in 2005, 2010 and 2010",
+                 "sometimes_treated" = "Sometimes treated (treat_05 + treat_10 + treat_15 > 0)",
+                 "always_treated" = "Always treated (quota in 2005, 10, & 15)"), 
+       replace = TRUE)
