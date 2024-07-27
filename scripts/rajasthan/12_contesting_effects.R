a69669dd7310cc049d8dd8a4bab3b1b5e45245f7
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2024-07-27 15:09:03 -0700

    rajasthan candidate quality

--- scripts/rajasthan/12_contesting_effects.R
+++ scripts/rajasthan/12_contesting_effects.R
@@ -92,6 +92,84 @@ etable(models_contests_list,
        replace = TRUE)
 
 
+
+
+
+# Additive Effect ---------------------------------------------------------
+
+
+m_prop <- feols(prop_women ~ treat_2005 + treat_2010 + treat_2015 , data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop)
+
+m_prop_dfe <- feols(prop_women ~ treat_2005 + treat_2010 + treat_2015  | district_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_dfe)
+
+m_prop_psfe <- feols(prop_women ~ treat_2005 + treat_2010 + treat_2015  | district_2020 + ps_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_psfe)
+
+m_prop_gpfe <- feols(prop_women ~ treat_2005 + treat_2010 + treat_2015  | I(paste0(district_2020, ps_2020)),  data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_gpfe)
+
+models_add_list <- list(m_prop, m_prop_dfe, m_prop_psfe, m_prop_gpfe)
+
+
+etable(models_add_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = "tables/raj_add_contest.tex",
+       dict = c( 'treat_2005' = "Treatment 2005",
+                 'prop_women' == "Proportion of women running in an open seat",
+                 'treat_2010' = "Treatment  2010",
+                 'treat_2015' = "Treatment 2015",
+                 'district_2020' = "District 2020",
+                 'I(paste0(district_2020, ps_2020))' = "(District, Samiti)",
+                 'ps_2020' = "Panchayat Samiti 2020",
+                 'treat_2020' = "Treatment 2020"),
+       se.row=FALSE,
+       replace = TRUE)
+
+
+
+# Multiplicative Effect ---------------------------------------------------------
+
+
+m_prop_mul <- feols(prop_women ~ treat_2005 * treat_2010 * treat_2015 , data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_mul)
+
+m_prop_mul_dfe <- feols(prop_women ~ treat_2005 * treat_2010 * treat_2015  | district_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_mul_dfe)
+
+m_prop_mul_psfe <- feols(prop_women ~ treat_2005 * treat_2010 * treat_2015  | district_2020 + ps_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_mul_psfe)
+
+m_prop_mul_gpfe <- feols(prop_women ~ treat_2005 * treat_2010 * treat_2015  | I(paste0(district_2020, ps_2020)),  data = filter(raj_panch, treat_2020 == 0))
+summary(m_prop_mul_gpfe)
+
+models_mul_list <- list(m_prop_mul, m_prop_mul_dfe, m_prop_mul_psfe, m_prop_mul_gpfe)
+
+
+etable(models_mul_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = "tables/raj_mul_contest.tex",
+       dict = c( 'treat_2005' = "Treatment 2005",
+                 'prop_women' == "Proportion of women running in an open seat",
+                 'treat_2010' = "Treatment  2010",
+                 'treat_2015' = "Treatment 2015",
+                 'treat_2020' = "Treatment 2020",
+                 'district_2020' = "District 2020",
+                 'I(paste0(district_2020, ps_2020))' = "(District, Samiti)",
+                 'ps_2020' = "Panchayat Samiti 2020",
+                 "treatment_categoryAlways Treated" = " Max Contrast ($WWW$ v. $OO0$)", 
+                 "never_treated" = "Never treated in 2005, 2010 and 2010",
+                 "sometimes_treated" = "Sometimes treated (treat_05 + treat_10 + treat_15 > 0)",
+                 "always_treated" = "Always treated (quota in 2005, 10, & 15)"), 
+       se.row=FALSE,
+       replace = TRUE)
+
+
 # At least One women Runs Models ------------------------------------------
 
 
@@ -134,4 +212,76 @@ etable(models_contests_list,
                  "sometimes_treated" = "Sometimes treated (treat_05 + treat_10 + treat_15 > 0)",
                  "always_treated" = "Always treated (quota in 2005, 10, & 15)"), 
        replace = TRUE)
+
+
+
+# Additive Effect ---------------------------------------------------------
+
+
+m_oneW_mul <- feols(I(women_present) ~ treat_2005 + treat_2010 + treat_2015 , data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_mul)
+
+m_oneW_mul_dfe <- feols(I(women_present) ~ treat_2005 + treat_2010 + treat_2015  | district_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_mul_dfe)
+
+m_oneW_mul_psfe <- feols(I(women_present) ~ treat_2005 + treat_2010 + treat_2015  | district_2020 + ps_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_mul_psfe)
+
+m_oneW_mul_gpfe <- feols(I(women_present) ~ treat_2005 + treat_2010 + treat_2015  | I(paste0(district_2020, ps_2020)),  data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_mul_gpfe)
+
+m_oneW_mul_list <- list(m_oneW_mul, m_oneW_mul_dfe, m_oneW_mul_psfe, m_oneW_mul_gpfe)
+
+
+etable(m_oneW_mul_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $\times $ ",
+       file = "tables/raj_oneW_add.tex",
+       dict = c( 'treat_2005' = "Treatment 2005",
+                 'I(women_present)' == "At least one woman running in an open seat",
+                 'treat_2010' = "Treatment  2010",
+                 'treat_2015' = "Treatment 2015",
+                 'district_2020' = "District 2020",
+                 'I(paste0(district_2020, ps_2020))' = "(District, Samiti)",
+                 'ps_2020' = "Panchayat Samiti 2020",
+                 'treat_2020' = "Treatment 2020"),
+       se.row=FALSE,
+       replace = TRUE)
+
+
+
+# Multiplicative Effect ---------------------------------------------------------
+
+m_oneW <- feols(I(women_present) ~ treat_2005 * treat_2010 * treat_2015 , data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW)
+
+m_oneW_dfe <- feols(I(women_present) ~ treat_2005 * treat_2010 * treat_2015  | district_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_dfe)
+
+m_oneW_psfe <- feols(I(women_present) ~ treat_2005 * treat_2010 * treat_2015  | district_2020 + ps_2020,  data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_psfe)
+
+m_oneW_gpfe <- feols(I(women_present) ~ treat_2005 * treat_2010 * treat_2015  | I(paste0(district_2020, ps_2020)),  data = filter(raj_panch, treat_2020 == 0))
+summary(m_oneW_gpfe)
+
+models_mul_list <- list(m_oneW, m_oneW_dfe, m_oneW_psfe, m_oneW_gpfe)
+
+
+etable(models_mul_list, 
+       tex = TRUE, 
+       style.tex = style.tex("aer",model.format = "[i]",depvar.style = "*"),
+       interaction.combine = " $ \\times $ ",
+       file = "tables/raj_oneW_mul.tex",
+       dict = c( 'treat_2005' = "Treatment 2005",
+                 'I(women_present)' == "At least one woman running in an open seat",
+                 'treat_2010' = "Treatment  2010",
+                 'treat_2015' = "Treatment 2015",
+                 'district_2020' = "District 2020",
+                 'I(paste0(district_2020, ps_2020))' = "(District, Samiti)",
+                 'ps_2020' = "Panchayat Samiti 2020",
+                 'treat_2020' = "Treatment 2020"),
+       se.row=FALSE,
+       replace = TRUE)
+
 
---
+++ scripts/uttarakhand/02_uk_short_term.R
@@ -0,0 +1,85 @@
+# Load libs.
+library(readr)
+library(arrow)
+library(tidyverse)
+library(stringi)
+library(kableExtra)
+library(here)
+library(fixest)
+
+# uk_all Anaysis ----------------------------------------------------------
+
+uk_all <- read_parquet("/Users/varun/Library/CloudStorage/Dropbox/USC/quota_raj/data/uttarakhand/uk_all_fuzzy.parquet")
+
+
+# Load libs.
+library(readr)
+library(arrow)
+library(tidyverse)
+library(stringi)
+library(kableExtra)
+library(here)
+library(fixest)
+
+
+# uk_all Anaysis ----------------------------------------------------------
+
+uk_all <- read_parquet("data/up/uk_all_fuzzy.parquet")
+
+
+# Reservation_Status -----------------------------------------------------
+
+# Treatment dummies
+uk_all <- uk_all %>%
+     mutate(
+          treat_2005 = ifelse(grepl("Female", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          treat_2010 = ifelse(grepl("Female", gp_res_status_fin_eng_2010, ignore.case = TRUE), 1, 0), 
+          treat_2015 = ifelse(grepl("Female", gp_reservation_status_eng_2015, ignore.case = TRUE), 1, 0),
+          treat_2021 = ifelse(grepl("Female", gp_reservation_status_eng_2021, ignore.case = TRUE), 1, 0),
+          
+          obc_2005 = ifelse(uk_all$gp_res_status_fin_eng_2005 %in% c("Other Backward Class - Female"), 1, 0),
+          obc_2010 = ifelse(uk_all$gp_res_status_fin_eng_2010 %in% c("Other Backward Class - Female"), 1, 0),
+          obc_2015 = ifelse(uk_all$gp_reservation_status_eng_2015 %in% c("Other Backward Class Female"), 1, 0),
+          obc_2021 = ifelse(uk_all$gp_reservation_status_eng_2021 %in% c("Other Backward Class Female"), 1, 0),
+          
+          dalit_2005 = ifelse(uk_all$gp_res_status_fin_eng_2005 %in% c("Scheduled Caste - Female", "Scheduled Tribe - Female"), 1, 0),
+          dalit_2010 = ifelse(uk_all$gp_res_status_fin_eng_2010 %in% c("Scheduled Caste - Female", "Scheduled Tribe - Female"), 1, 0),
+          dalit_2015 = ifelse(uk_all$gp_reservation_status_eng_2015 %in% c("Scheduled Caste - Female", "Scheduled Tribe - Female"), 1, 0),
+          dalit_2020 = ifelse(uk_all$gp_reservation_status_eng_2021 %in% c("Scheduled Caste Female", "Scheduled Tribe Female"), 1, 0),
+          
+          twice_treated = ifelse((treat_2005 + treat_2010) == 2, 1, 0),
+          never_treated = ifelse((treat_2005 + treat_2010 ) == 0, 1, 0),
+          sometimes_treated = ifelse((treat_2005 + treat_2010 ) > 0, 1, 0),
+          
+          count_treated = (treat_2005 + treat_2010 ),
+          
+          once = ifelse((treat_2005 + treat_2010 ) == 1, 1, 0),
+          
+          inter_always_treated = ifelse((treat_2010 == 1) & (treat_2005 == 1), 1, 0),
+          inter_sometimes_treated = ifelse((treat_2010 == 1) | (treat_2005 == 1), 1, 0),
+          inter_never_treated = ifelse(treat_2005 + treat_2010 == 0, 1, 0),
+          treat_all = paste(treat_2005, treat_2010, sep = "_"),
+          
+          all_sc_2005 = ifelse(grepl("Scheduled", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          all_sc_2010 = ifelse(grepl("Scheduled", gp_res_status_fin_eng_2010, ignore.case = TRUE), 1, 0),
+          all_sc_2015 = ifelse(grepl("Scheduled", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          all_sc_2020 = ifelse(grepl("Scheduled", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          
+          all_obc_2005 = ifelse(grepl("Backward", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          all_obc_2010 = ifelse(grepl("Backward", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          all_obc_2015 = ifelse(grepl("Backward", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          all_obc_2020 = ifelse(grepl("Backward", gp_res_status_fin_eng_2005, ignore.case = TRUE), 1, 0),
+          
+          fe_key_2010 = paste(district_name_eng_2010, block_name_eng_2010),
+          cluster_key_2010 = paste(district_name_eng_2010, block_name_eng_2010, gp_name_eng_2010),
+          
+          fe_key_2015 = paste(district_name_eng_2015,block_name_eng_2015),
+          cluster_key_2015 = paste(district_name_eng_2015,block_name_eng_2015, gp_name_eng_2015),
+          
+          fe_key_2021 = paste(district_name_eng_2021, block_name_eng_2021),
+          cluster_key_2021 = paste(district_name_eng_2021, block_name_eng_2021, gp_name_eng_2021))
+
+
+save(uk_all, file = "data/up/uk_all_fuzzy_recoded.RData")
+
+
