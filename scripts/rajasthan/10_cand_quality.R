aafe548c6d4dc9ff1f561f954c68db8ccd1cc210
shenoyvarun86 <42848747+shenoyvarun86@users.noreply.github.com>
2024-10-10 12:15:12 -0700

    minor_clean
    10_cand_quality minor edits

--- scripts/rajasthan/10_cand_quality.R
+++ scripts/rajasthan/10_cand_quality.R
@@ -126,6 +126,99 @@ raj_win <- raj_win %>%
 # Merge datasets
 winner_ttest <- raj_win %>%
      inner_join(raj_summ, by = "key_2020")
+rm(raj_summ)
+rm(raj_win)
+
+winner_ttest_clean <- winner_ttest %>%
+     mutate(
+          treat = ifelse(categoryofgrampanchayat %in% c("general (woman)", "obc (woman)", "sc (woman)", "st (woman)"), 1, 0),
+          win = winsor(as.integer(totalvalueofcapitalassets), trim = 0.1, na.rm = TRUE),
+          grad_status = ifelse(educationstatus %in% c("postgraduate", "graduate", "professional graduate", "professional post graduate"), 1, 0)
+     ) %>%
+     filter(
+          !is.na(age), 
+          !is.na(childrenbefore27111995), 
+          !is.na(childrenonorafter28111995),
+          !is.na(win), 
+          !is.na(grad_status),
+          !is.na(contestingcandidateoccupation)
+     ) %>%
+     mutate(total_children = as.integer(childrenbefore27111995) + as.integer(childrenonorafter28111995)) %>% 
+     mutate(age = as.integer(age),
+            total_children = as.integer(total_children),
+            grad_status = as.integer(grad_status))
+
+age_test <- t.test(age ~ treat, data = winner_ttest_clean)
+total_children_test <- t.test(total_children ~ treat, data = winner_ttest_clean)
+grad_status_test <- t.test(grad_status ~ treat, data = winner_ttest_clean)
+
+#unemployed bin
+
+winner_ttest_clean <- winner_ttest_clean %>%
+     mutate(unemployed = ifelse(contestingcandidateoccupation == "unemployed", 1, 0))
+
+unemployed_test <- t.test(unemployed ~ treat, data = winner_ttest_clean)
+assets_test <- t.test(win ~ treat, data = winner_ttest_clean)
+
+
+winner_results <- data.frame(
+     Variable = c("Age", "Total Children", "Graduation Status", "Unemployed", "Assets"),
+     Mean_Non_Gender_Quota = c(age_test$estimate[1], total_children_test$estimate[1],
+                               grad_status_test$estimate[1], unemployed_test$estimate[1],
+                               assets_test$estimate[1]),
+     Mean_Gender_Quota = c(age_test$estimate[2], total_children_test$estimate[2],
+                           grad_status_test$estimate[2], unemployed_test$estimate[2],
+                           assets_test$estimate[2]),
+     Difference = c(age_test$estimate[2] - age_test$estimate[1],
+                    total_children_test$estimate[2] - total_children_test$estimate[1],
+                    grad_status_test$estimate[2] - grad_status_test$estimate[1],
+                    unemployed_test$estimate[2] - unemployed_test$estimate[1],
+                    assets_test$estimate[2] - assets_test$estimate[1]),
+     t_value = c(age_test$statistic, total_children_test$statistic,
+                 grad_status_test$statistic, unemployed_test$statistic,
+                 assets_test$statistic),
+     Std_Error = c(age_test$stderr, total_children_test$stderr,
+                   grad_status_test$stderr, unemployed_test$stderr,
+                   assets_test$stderr),
+     # p_value = c(age_test$p.value, total_children_test$p.value,
+     #             grad_status_test$p.value, unemployed_test$p.value,
+     #             assets_test$p.value),
+     Significance = c(ifelse(age_test$p.value < 0.001, "***", 
+                             ifelse(age_test$p.value < 0.01, "**", 
+                                    ifelse(age_test$p.value < 0.05, "*", "ns"))),
+                      ifelse(total_children_test$p.value < 0.001, "***", 
+                             ifelse(total_children_test$p.value < 0.01, "**", 
+                                    ifelse(total_children_test$p.value < 0.05, "*", "ns"))),
+                      ifelse(grad_status_test$p.value < 0.001, "***", 
+                             ifelse(grad_status_test$p.value < 0.01, "**", 
+                                    ifelse(grad_status_test$p.value < 0.05, "*", "ns"))),
+                      ifelse(unemployed_test$p.value < 0.001, "***", 
+                             ifelse(unemployed_test$p.value < 0.01, "**", 
+                                    ifelse(unemployed_test$p.value < 0.05, "*", "ns"))),
+                      ifelse(assets_test$p.value < 0.001, "***", 
+                             ifelse(assets_test$p.value < 0.01, "**", 
+                                    ifelse(assets_test$p.value < 0.05, "*", "ns")))))
+
+# Print results
+print(winner_results)
+
+
+output_file <- winner_results %>%
+     kable("latex", 
+           caption = "T-Test Results: Rajasthan Winners", 
+           col.names = c("Variable", "Mean Non-Gender Quota", "Mean Gender Quota", "Difference", "t-value", "Std. Error", "Significance"), 
+           digits = 3) %>%
+     row_spec(0, bold = TRUE)  
+output_path <- "tables/winner_t_test_results.tex"
+save_kable(output_file, file = output_path)
+
+
+
+
+
+
+
+
 
 # Prepare summary statistics - Panel 1
 winner_tab1 <- winner_ttest %>%
@@ -143,44 +236,4 @@ winner_ttest <- winner_ttest %>%
      mutate(win = winsor(as.integer(totalvalueofcapitalassets), trim = 0.1, na.rm = TRUE))
 
 
-
-
-# 2020 Close elections? ---------------------------------------------------
-
-
-close_elec <- readr::read_csv("data/rajasthan/sarpanch_election_data/background/WinnerSarpanch_2020.csv")
-names(close_elec) <- tolower(names(close_elec))
-close_elec <- close_elec %>%
-     mutate_all(tolower) %>%
-     rename(key = key_2020) %>%
-     filter(electiontype != "by election") %>%
-     distinct(key, .keep_all = TRUE) %>% 
-     mutate(margin = (as.integer(votesecurebywinner) - as.integer(votesecurebyrunnerup))/as.integer(totalvalidvotes),
-            winner_percentage = (as.integer(votesecurebywinner)/as.integer(totalvalidvotes)))%>% 
-     filter(margin > 0) %>% 
-     # new_key = paste0(district,panchayatsamiti)) %>% 
-     mutate(across(where(is.numeric), ~ round(.x, 3))) %>% 
-     group_by(district) %>% 
-     summarize(
-          avg_valid_votes = mean(as.numeric(totalvalidvotes), na.rm = TRUE),
-          avg_winner_vote_percentage = mean(winner_percentage, na.rm = TRUE), 
-          lowest_percentage = min(winner_percentage, na.rm = TRUE), 
-          highest_percentage = max(winner_percentage, na.rm = TRUE), 
-          median_percentage = median(winner_percentage, na.rm = TRUE), 
-          avg_winner_margin = mean(margin, na.rm = TRUE), 
-          closest_margin = min(margin, na.rm = TRUE), 
-          median_margin = median(margin, na.rm = TRUE), 
-          largest_margin = max(margin, na.rm = TRUE) 
-     ) %>% 
-     mutate(across(where(is.numeric), ~ round(.x, 3)),
-            avg_valid_votes = round(avg_valid_votes, 0),
-            closest_margin = round(closest_margin, 3) )
-
-library(kableExtra)
-summary_table <- kable(close_elec, format = "latex", booktabs = TRUE, 
-                       caption = "Summary Statistics for Election Metrics by District") %>%
-     kable_styling(latex_options = c("hold_position")) 
-
-
-writeLines(summary_table, "tables/close_elec.tex")
 
